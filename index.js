cf3658e32dd60f046bab7d620fa95aeaca10cd5d
+branden <+branden_ott@outlook.com+>
2021-11-10 17:05:58 -0500

    DOM lab compleated. Doses not pass test but works

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const renderedText = "This is really cool!"
+    document.getElementById('text').innerText = renderedText
+  });
+
